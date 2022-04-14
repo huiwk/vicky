bbcc7d004c9214b1bc31bbe9ac33bcac87bcd52c
huiwk <80872265+huiwk@users.noreply.github.com>
2022-04-15 00:40:15 +0800

    Update dts_align.r

--- vicky/R/dts_align.r
+++ vicky/R/dts_align.r
@@ -1,3 +1,24 @@
+#' To smoothen time series for drug data
+#'
+#'
+#' @param dat your drug data file, of \code{data.frame} structure.
+#'
+#' @param type Element for by group smoothing, optional.
+#' @param refid Your "Refkey" variable name, can use quotes or apostrophe to quote the name. If name is exactly "Reference Key", can ignore it, that is optional
+#' @param startid Your "PrescriptionStartDate" variable name, can use quotes or apostrophe to quote the name. If name is exactly "Prescription Start Date", can ignore it, that is optional
+#' @param endid Your "PrescriptionEndDate" variable name, can use quotes or apostrophe to quote the name. If name is exactly "Prescription End Date", can ignore it, that is optional
+#'
+#' @examples \dontrun{
+#' # style 1
+#' # vicky::dts_algin(dat=Drug,type="Drug",refid="ReferenceKey",startid="Prescription Start Date",endid="Prescription End Date")
+#' # style 2
+#' # vicky::dts_algin(dat=Drug,type=`Drug`,refid=`ReferenceKey`,startid=`Prescription Start Date`,endid=`Prescription End Date`)
+#' # style 3
+#' # vicky::dts_algin(dat=Drug,type="Drug")
+#' # style 4
+#' # vicky::dts_algin(dat=Drug)}
+#' @export
+
 dts_align<-function(dat,type,refid=`Reference Key`,startid=`Prescription Start Date`,endid=`Prescription End Date`){
   type<-if(hasArg(type)) ensym(type)
   refid<-ensym(refid)
