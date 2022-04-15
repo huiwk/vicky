66ddb0cfbbc5b1672d6e2e4a68d709b45198cd70
huiwk <80872265+huiwk@users.noreply.github.com>
2022-04-15 11:46:31 +0800

    Update dts_align.r

--- vicky/R/dts_align.r
+++ vicky/R/dts_align.r
@@ -28,10 +28,10 @@ dts_align<-function(dat,type,refid=`Reference Key`,startid=`Prescription Start D
   startidname<-expr(!!startid)%>%as.character
   endidname<-expr(!!endid)%>%as.character
   dat%>%distinct(.keep_all=T)%>%arrange(across(contains(refidname)|contains(startidname)|contains(endidname)))%>%{if(!is.null(type)) group_by(.,!!refid,!!type) else group_by(.,!!refid)}%>%
-    mutate(`Last End Date`=lag(!!endid)%>%as.Date(.,origin="1900-01-01"),
-           `Min Start Date`=min(!!startid)%>%as.Date(.,origin="1900-01-01"),
-           `Accumulated Last End Date`=cummax((`Last End Date`%>%as.numeric%>%ifelse(is.na(.),-999,.)))%>%ifelse(.==-999,min(!!endid),.)%>%as.Date(.,origin="1900-01-01"),
-           `Aligned Prescription Start Date`=ifelse((startid<`Accumulated Last End Date`)&(startid>`Min Start Date`),`Accumulated Last End Date`,!!startid)%>%as.Date(.,origin="1900-01-01"),
+    mutate(`Last End Date`=lag(!!endid)%>%as.Date(),
+           `Min Start Date`=min(!!startid)%>%as.Date(),
+           `Accumulated Last End Date`=cummax((`Last End Date`%>%as.numeric%>%ifelse(is.na(.),-999,.)))%>%ifelse(.==-999,min(!!endid),.)%>%as.Date(),
+           `Aligned Prescription Start Date`=ifelse((startid<`Accumulated Last End Date`)&(startid>`Min Start Date`),`Accumulated Last End Date`,!!startid)%>%as.Date(),
            `Drug Duration`=time_length(interval(`Aligned Prescription Start Date`,!!endid),"day"),
            `Accumulated Drug Duration`=cumsum(ifelse(`Drug Duration`<0,0,`Drug Duration`)))
 }
