d6e8ac66874eee1313d8121533db0e4884a64b1f
huiwk <80872265+huiwk@users.noreply.github.com>
2022-04-27 14:53:49 +0800

    Add files via upload

---
+++ vicky/R/utils.r
@@ -0,0 +1,15 @@
+`%clip%`<-function(){
+  x<-gsub('\"',"",readClipboard(raw=F))
+  rstudioapi::insertText(paste(x,"\n"))
+  x
+ }
+ saveCohort<-function(cohort,fp){
+  cohort%>%fwrite(paste0(fp,"\\cohort_",format(Sys.time(),"%d%m%Y"),'.csv'))
+}
+readCohort<-function(path)
+{
+  details<-list.files(path,full.names=TRUE)%>%file.info
+  details<-details[with(details,order(as.POSIXct(mtime),decreasing=TRUE)),]
+  files=rownames(details)
+  cohort<<-fread(files[1])%>%tibble%>%mutate_all(as.character)
+}
