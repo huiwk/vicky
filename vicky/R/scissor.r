b8c4dcebc5317a816571be86910ae751bcbff2e3
huiwk <80872265+huiwk@users.noreply.github.com>
2022-04-28 14:59:25 +0800

    dataset segmentation

---
+++ vicky/R/scissor.r
@@ -0,0 +1,12 @@
+
+scissor<-function(fp,RefkeyIDthread1="Reference",RefkeyIDthread2="Key",IDvars=list(Lab=c("LIS"),Med=c("Disp","Pres"),Dx=c("diag"),
+  Px=c("proc"),Demo=c("birth","death","sex")),out_fp=getwd(),milliseconds=6){
+  options(digits.secs=milliseconds)
+  dat<-read_parquet(fp)%>%mutate_all(as.character)
+  if(!grepl(r"(/$)",out_fp)) {out_fp<-paste0(out_fp,r"(/)",collapse="|")}
+  Refkeypos<-grep(paste0(RefkeyIDthread1,".*",RefkeyIDthread2),names(dat))%>%{if(length(.)>0) . else 0}
+  map(names(IDvars),~dir.create(paste0(out_fp,.x),showWarnings=FALSE))
+  map2(IDvars,names(IDvars),~grep(paste0(.x,collapse="|"),names(dat),ignore.case=TRUE)%>%
+         {if(length(.)>0) select(dat,all_of(Refkeypos),min(.):ncol(dat))%>%
+             write_parquet(paste0(out_fp,.y,"/",format(Sys.time(), "%Y%m%d_%H%M%OS"),".parquet"))})
+}
