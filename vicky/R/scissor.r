0e0a4b70a74c9485f273c99e63eca94a826aad56
huiwk <80872265+huiwk@users.noreply.github.com>
2022-04-28 23:56:57 +0800

    Update scissor.r

--- vicky/R/scissor.r
+++ vicky/R/scissor.r
@@ -3,8 +3,8 @@ scissor<-function(fp,RefkeyIDthread1="Reference",RefkeyIDthread2="Key",IDvars=li
   Px=c("proc"),Demo=c("birth","death","sex")),out_fp=getwd(),milliseconds=6){
   options(digits.secs=milliseconds)
   dat<-read_parquet(fp)%>%mutate_all(as.character)
-  if(!grepl(r"(/$)",out_fp)) {out_fp<-paste0(out_fp,r"(/)",collapse="|")}
-  Refkeypos<-grep(paste0(RefkeyIDthread1,".*",RefkeyIDthread2),names(dat))%>%{if(length(.)>0) . else 0}
+  if(!grepl(r"((/$)|(\\$))",y)) {out_fp<-paste0(out_fp,r"(/)",collapse="|")}
+  Refkeypos<-grep(paste0(RefkeyIDthread1,".*",RefkeyIDthread2),names(dat),ignore.case=TRUE)%>%{if(length(.)>0) . else 0}
   map(names(IDvars),~dir.create(paste0(out_fp,.x),showWarnings=FALSE))
   map2(IDvars,names(IDvars),~grep(paste0(.x,collapse="|"),names(dat),ignore.case=TRUE)%>%
          {if(length(.)>0) select(dat,all_of(Refkeypos),min(.):ncol(dat))%>%
