5f2192d469ee72b4046fa18e4a22cce26834a49a
huiwk <80872265+huiwk@users.noreply.github.com>
2022-05-13 12:27:03 +0800

    Update fine.r

--- vicky/R/fine.r
+++ vicky/R/fine.r
@@ -1,15 +1,16 @@
 
-scissor<-function(fp,RefkeyIDthread1="Reference",RefkeyIDthread2="Key",IDvars=list(Lab=c("LIS"),Med=c("Disp","Pres"),Dx=c("diag"),
-                                                                                   Px=c("proc"),Demo=c("birth","death","sex")),out_fp=getwd(),milliseconds=6,as.parquet=FALSE){
+scissor<-function(fp,RefkeyIDthread1="Reference",RefkeyIDthread2="Key",
+                  IDvars=list(Lab=c("LIS"),Med=c("Disp","Pres"),Dx=c("diag"),
+                              Px=c("proc"),Demo=c("birth","death","sex")),out_fp=getwd(),milliseconds=6,as.parquet=FALSE){
   options(digits.secs=milliseconds)
-  dat<-read_parquet(fp)%>%mutate_all(as.character)
+  dat<-opener(fp)%>%mutate_all(as.character)
   if(!grepl(r"((/$)|(\\$))",out_fp)) {out_fp<-paste0(out_fp,r"(/)",collapse="|")}
   Refkeypos<-grep(paste0(RefkeyIDthread1,".*",RefkeyIDthread2),names(dat),ignore.case=TRUE)%>%{if(length(.)>0) . else 0}
   map(names(IDvars),~dir.create(paste0(out_fp,.x),showWarnings=FALSE))
   map2(IDvars,names(IDvars),~grep(paste0(.x,collapse="|"),names(dat),ignore.case=TRUE)%>%
          {if(length(.)>0) {tmp<-select(dat,all_of(Refkeypos),min(.):ncol(dat))
          if (dim(tmp)[1]>0&as.parquet==TRUE) tmp%>%write_parquet(paste0(out_fp,.y,"/",format(Sys.time(), "%Y%m%d_%H%M%OS"),".parquet"))
-         if (dim(tmp)[1]>0&as.parquet==FALSE) tmp%>%fwrite(paste0(out_fp,.y,"/",format(Sys.time(), "%Y%m%d_%H%M%OS"),".csv"))
+         if (dim(tmp)[1]>0&as.parquet==FALSE) tmp%>%fwrite(paste0(out_fp,.y,"/",format(Sys.time(), "%Y%m%d_%H%M%OS"),".csv"),sep=";;;;;")
          }
          } 
   )
