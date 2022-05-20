cdf0f40c20920f8d8c847a8c4b9ca8479af29c6d
huiwk <80872265+huiwk@users.noreply.github.com>
2022-05-20 22:42:28 +0800

    Update scratch.r

--- vicky/scratch.r
+++ vicky/scratch.r
@@ -54,23 +54,31 @@ is.normal<-function(vec){
 # weighted mean + sd ------------------------------------------------------
 
 weighted.mean.sd<-function(vec,flag=is.normal(vec),weight=NULL,fieldwidth=5,precision=2,p1="±",p2=""){
-  map2(vec,flag,~{
+  weight<-vec$weight
+  vec<-vec%>%select(-weight)
+  stats<-map2(vec,flag,~{
     eachvec<-{if(is.data.frame(.x)) .x%>%reduce(c) else .x}
     eachflag<-.y
     if(!length(eachflag)) return(NULL)
     if(eachflag){
       c(wtd.mean(eachvec,weight),sqrt(wtd.var(eachvec,weight)))%>%
-        map_chr(~sprintf(paste0("%",fieldwidth,".",precision,"f"),.x)%>%trimws)%>%{paste0(.[1],p1,.[2],p2)}
+        map_chr(~sprintf(paste0("%",fieldwidth,".",precision,"f"),.x)%>%trimws)%>%
+        {paste0(.[1],p1,.[2],p2)}
     }
-    
-  })
-}
+  }
+  )
+  tibble(variables=names(vec)[map_lgl(stats,~is.null(.x))%>%`!`%>%which],
+         statistics=stats%>%reduce(c))
+  }
 
 # weighted iqr ------------------------------------------------------------
 
 weighted.inter.quartile.range<-function(vec,flag=NULL,weight=NULL,fieldwidth=5,precision=2,p1="[",p2=",",p3="]"){
-  flag<-{if(!length(flag)) {is.normal(vec)%>%map(~{if(is.null(.x)) FALSE else if (.==FALSE) TRUE else .})} else flag}
-  map2(vec,flag,~{
+  weight<-vec$weight
+  vec<-vec%>%select(-weight)
+  flag<-{if(!length(flag)) {is.normal(vec)%>%map(~{if(is.null(.x)) FALSE else if (.==FALSE) TRUE else FALSE})} else flag}
+  flag
+  stats<-map2(vec,flag,~{
     eachvec<-{if(is.data.frame(.x)) .x%>%reduce(c) else .x}
     eachflag<-.y
     if(!length(eachflag)) return(NULL)
@@ -78,14 +86,18 @@ weighted.inter.quartile.range<-function(vec,flag=NULL,weight=NULL,fieldwidth=5,p
       c(wtd.quantile(eachvec,weight,probs=c(.25,.5,.75)))%>%
         map_chr(~sprintf(paste0("%",fieldwidth,".",precision,"f"),.x)%>%trimws)%>%{paste0(.[1],p1,.[2],p2,.[3],p3)}
     }
-    
+
   })
+  tibble(variables=names(vec)[map_lgl(stats,~is.null(.x))%>%`!`%>%which],
+         statistics=stats%>%reduce(c))
 }
 
 # weighted count + pct ---------------------------------------------------- (continue)
 
 weighted.cnt.pct<-function(vec,flag=is.categorical.variable(vec),weight=NULL,fieldwidth=5,precision=2,p1="(",p2="%)"){
-  map2(vec,flag,~{
+    weight<-vec$weight
+    vec<-vec%>%select(-weight)
+    stats<-map2(vec,flag,~{
     eachvec<-{if(is.data.frame(.x)) reduce(.x,c,na.rm=T) else .x}%>%{if(!is.factor(.)) as.factor(.) else .}
     eachflag<-.y
     eachlevel<-eachvec%>%levels()
@@ -100,11 +112,14 @@ weighted.cnt.pct<-function(vec,flag=is.categorical.variable(vec),weight=NULL,fie
       })
     }
   })
+    stats%>%rbindlist(idcol="variables")%>%tibble
 }
 
 # missing -----------------------------------------------------------------
 weighted.missing.pct<-function(vec,flag=ncol(vec),weight=NULL,fieldwidth=5,precision=2,p1="(",p2="%)"){
-  map2(vec,flag,~{
+  weight<-vec$weight
+  vec<-vec%>%select(-weight)
+  stats<-map2(vec,flag,~{
     eachvec<-{if(is.data.frame(.x)) reduce(.x,c,na.rm=T) else .x}%>%{if(!is.factor(.)) as.factor(.) else .}
     eachflag<-.y
     eachlevel<-eachvec%>%levels()
@@ -115,6 +130,7 @@ weighted.missing.pct<-function(vec,flag=ncol(vec),weight=NULL,fieldwidth=5,preci
       pct<-(cnt/denom)*100
       c(cnt,pct)%>%map_chr(~sprintf(paste0("%",fieldwidth,".",precision,"f"),.x)%>%trimws)%>%{tibble(levels=NA,statistic=paste0(.[1],p1,.[2],p2))}    }
   })
+  stats%>%rbindlist(idcol="variables")%>%tibble
 }
 
 
@@ -202,6 +218,45 @@ cnt.pct.test.chi<-function(vec,groupID,flag=is.categorical.variable(vec%>%select
   })
 }
 
+
+# Balance statistics ------------------------------------------------------
+
+ASMD<-function(vec,groupID,weight=NULL,fieldwidth=5,precision=4,method="ATT",twang=TRUE){
+  paired.dat<-by(vec,vec[[groupID]],identity)%>%{
+    subsets<-.
+    map((combn(seq_len(vec[[groupID]]%>%as.factor%>%nlevels),2))%>%
+          data.frame%>%as.list,
+        ~{subsets[c(.x)]%>%rbindlist(fill=T)})
+  }
+  dnames<-map_chr(paired.dat,~{
+    d<-.
+    d[[groupID]]%>%as.factor()%>%levels%>%{paste(.,collapse=" vs ")}
+  })
+  map(paired.dat,~{
+      pair.dat<-.x[complete.cases(.x),]
+      treat.var<-pair.dat[[groupID]]%>%as.factor()
+      s.d.denom<-case_when(
+        is.null(weight)|twang==TRUE~"all",
+        method=="ATT"~"treated",
+        method=="ATE"~"pooled"
+        )
+      
+      bal.tab.weight<-if(!is.null(weight)) pair.dat$weight else rep(1,times=nrow(pair.dat))
+
+      bal.tab(pair.dat%>%select(-all_of(groupID)),treat=treat.var,bindary="std",
+            weights=bal.tab.weight,s.d.denom=s.d.denom)%>%{
+              asmd<-.$Balance%>%
+                  .$Diff.Adj%>%map_chr(~{if(is.null(.)) "N/A" else {if(abs(.)<0.001) "<0.001" else sprintf(paste0("%",fieldwidth,".",precision,"f"),abs(.))%>%trimws}})
+              conames<-.[[1]]%>%rownames
+              tibble(variables=conames,ASMD=asmd)
+            }
+      })%>%set_names(dnames)
+}
+
+
+# Warp-up Unweighted ------------------------------------------------------
+
+Unweighted.stats<-
 # test case ---------------------------------------------------------------
 
 max.categories<-10
@@ -220,6 +275,22 @@ inter.quartile.range.test(cohort,groupID="mgroupid")
 cnt.pct.test.fisher(cohort,groupID="mgroupid")
 cnt.pct.test.chi(cohort,groupID="mgroupid")
 
+library(twang)
+data("AOD")
+cohort<-AOD
+g<-sample(c(0:1),size=nrow(cohort),prob=c(0.4,0.6),replace=TRUE)
+m<-sample(c(0:2),size=nrow(cohort),prob=c(0.3,0.4,0.3),replace=TRUE)
+cohort$groupid<-g
+cohort$mgroupid<-m
+cohort$weight<-runif(nrow(cohort))
+ASMD(cohort,groupID="groupid")
+
+by(cohort,cohort[["groupid"]],identity)%>%{
+  subsets<-.
+  map((combn(seq_len(cohort[["groupid"]]%>%as.factor%>%nlevels),2))%>%
+        data.frame%>%as.list,
+      ~{subsets[c(.x)]%>%rbindlist(fill=T)})
+}
 
 
 bal.tab(covs, treat = lalonde$treat, weights = lalonde$att.weights)
